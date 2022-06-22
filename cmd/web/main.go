bdd301973959aa553cffcd9931f5b02c9a15af15
eric <eathom91@gmail.com>
2022-06-22 17:13:13 -0400

    Saved progress

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -18,21 +18,25 @@ func main() {
 	// Creating, parsing flags from CLI
 	addr := flag.String("addr", ":4000", "HTTP Network Address")
 	flag.Parse()
+
 	// Creating info, error loggers
 	infoLog := log.New(os.Stdin, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stdin, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	// Creating application obj with logger intialization.
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
 	}
+
 	// Creating the server with the port address, errorLogger, and mux with all our routes.
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
 		Handler:  app.routes(),
 	}
-	// Info logging, and begin server listening.
+
+	// Info logging, and running the server.
 	infoLog.Printf("Starting server on %s", *addr)
 	err := srv.ListenAndServe()
 	errorLog.Fatal(err)
