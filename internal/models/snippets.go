5fbd13889848d7f3566521ccc0f62427bff3428d
eric <eathom91@gmail.com>
2022-06-24 14:24:56 -0400

    Added custom no-record found error

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -41,7 +41,8 @@ func (m *SnippetModel) Insert(title string, content string, expires int) (int, e
 // Get This will return a specific snippet based on its id.
 func (m *SnippetModel) Get(id int) (*Snippet, error) {
 	stmt := `SELECT id, title, content, created, expires FROM snippets
-	WHERE expires > UTC_TIMESTAMP() AND id = ?`
+			 WHERE expires > UTC_TIMESTAMP() AND id = ?`
+
 	row := m.DB.QueryRow(stmt, id)
 	s := &Snippet{}
 	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
