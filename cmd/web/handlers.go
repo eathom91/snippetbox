eb737136b6264340bcf8834bf44021d195b67b19
eric <eathom91@gmail.com>
2022-06-24 17:01:24 -0400

    Implemented latest snippet functionality

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -6,7 +6,6 @@ import (
 	"errors"
 	"fmt"
 	"github.com/eathom91/snippetbox/internal/models"
-	"html/template"
 	"net/http"
 	"strconv"
 )
@@ -17,21 +16,31 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		app.notFound(w)
 		return
 	}
-	files := []string{
-		"./ui/html/pages/home.tmpl",
-		"./ui/html/pages/base.tmpl",
-		"./ui/html/partials/nav.tmpl",
-	}
-
-	ts, err := template.ParseFiles(files...)
+	snippets, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, err)
 		return
 	}
-	err = ts.ExecuteTemplate(w, "base", nil)
-	if err != nil {
-		app.serverError(w, err)
+
+	for _, snippet := range snippets {
+		fmt.Fprintf(w, "%+v\n", snippet)
 	}
+
+	//files := []string{
+	//	"./ui/html/pages/home.tmpl",
+	//	"./ui/html/pages/base.tmpl",
+	//	"./ui/html/partials/nav.tmpl",
+	//}
+	//
+	//ts, err := template.ParseFiles(files...)
+	//if err != nil {
+	//	app.serverError(w, err)
+	//	return
+	//}
+	//err = ts.ExecuteTemplate(w, "base", nil)
+	//if err != nil {
+	//	app.serverError(w, err)
+	//}
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
