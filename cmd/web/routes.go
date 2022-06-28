c8f12628fe4ef324710feba18bd9ef8959c40f63
eric <eathom91@gmail.com>
2022-06-27 20:50:27 -0400

    Added documentation

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,7 +1,7 @@
 package main
 
 // Routes.go contains all the logic for registering routes in a mux that is then returned.
-// Routes.go makes use of the handlers.go functions to initialize each route.
+// Routes.go makes use of the handlers.go functions to initialize each route
 import "net/http"
 
 func (app *application) routes() *http.ServeMux {
