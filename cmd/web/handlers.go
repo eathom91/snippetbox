fb5ab582f6611175dadb28dfbcb00fb3c3da5e63
eric <eathom91@gmail.com>
2022-06-27 20:44:39 -0400

    Added documentation

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,7 +1,7 @@
 package main
 
 // Handlers.go is a go file containing all the handler functions that will be associated
-// and used with app as the receiver.
+// and used with app as the receiver..
 import (
 	"errors"
 	"fmt"
