317edebaf5417d7341d8266a15abb56670be216f
eric <eathom91@gmail.com>
2022-07-04 15:59:31 -0400

    Updated/wired up home page to display latest snippest

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -24,26 +24,24 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	for _, snippet := range snippets {
-		fmt.Println(len(snippets))
-		fmt.Fprintf(w, "%+v\n", snippet)
+	files := []string{
+		"./ui/html/pages/home.html",
+		"./ui/html/pages/base.html",
+		"./ui/html/partials/nav.tmpl",
 	}
 
-	//files := []string{
-	//	"./ui/html/pages/home.html",
-	//	"./ui/html/pages/base.html",
-	//	"./ui/html/partials/nav.tmpl",
-	//}
-	//
-	//ts, err := template.ParseFiles(files...)
-	//if err != nil {
-	//	app.serverError(w, err)
-	//	return
-	//}
-	//err = ts.ExecuteTemplate(w, "base", nil)
-	//if err != nil {
-	//	app.serverError(w, err)
-	//}
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+	data := &templateData{
+		Snippets: snippets,
+	}
+	err = ts.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		app.serverError(w, err)
+	}
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
