8a28e9c39a6afd14be7069cc4ed61a8107818a68
eric <eathom91@gmail.com>
2022-07-22 13:25:19 -0400

    reinstalled justinas, refactored middleware chain in routes

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,7 +2,10 @@ package main
 
 // Routes.go contains all the logic for registering routes in a mux that is then returned.
 // Routes.go makes use of the handlers.go functions to initialize each route
-import "net/http"
+import (
+	"github.com/justinas/alice"
+	"net/http"
+)
 
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
@@ -14,5 +17,7 @@ func (app *application) routes() http.Handler {
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
-	return app.recoverPanic(app.logRequest(secureHeaders(mux)))
+	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+
+	return standard.Then(mux)
 }
