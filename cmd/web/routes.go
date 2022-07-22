6e5b29d6b8360a3fcd83cf4f2bad3a8104686313
eric <eathom91@gmail.com>
2022-07-22 15:29:43 -0400

    Revert "Updated routes to use new method based routing package"
    This reverts commit 27f1647fa7af352cbc9b95cac25867e400fef072.

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,23 +3,21 @@ package main
 // Routes.go contains all the logic for registering routes in a mux that is then returned.
 // Routes.go makes use of the handlers.go functions to initialize each route
 import (
-	"github.com/julienschmidt/httprouter"
 	"github.com/justinas/alice"
 	"net/http"
 )
 
 func (app *application) routes() http.Handler {
-	router := httprouter.New()
+	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static"))
-	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	router.HandlerFunc(http.MethodGet, "/", app.home)
-	router.HandlerFunc(http.MethodGet, "/snippet/view", app.snippetView)
-	router.HandlerFunc(http.MethodGet, "/snippet/create", app.snippetCreate)
-	router.HandlerFunc(http.MethodPost, "/snippet/create", app.snippetCreatPost)
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet/view", app.snippetView)
+	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
 	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 
-	return standard.Then(router)
+	return standard.Then(mux)
 }
--- go.mod
+++ go.mod
@@ -4,7 +4,4 @@ go 1.18
 
 require github.com/go-sql-driver/mysql v1.6.0
 
-require (
-	github.com/julienschmidt/httprouter v1.3.0 // indirect
-	github.com/justinas/alice v1.2.0 // indirect
-)
+require github.com/justinas/alice v1.2.0 // indirect
